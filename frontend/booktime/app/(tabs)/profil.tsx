ba952980ad08f5e776c3af89ec24bcb0162cc1ac
GabrielM1z <gabazoo.martinez@gmail.com>
2024-06-11 16:00:02 +0200

    commit

--- frontend/booktime/app/(tabs)/profil.tsx
+++ frontend/booktime/app/(tabs)/profil.tsx
@@ -1,20 +1,67 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
+// import des component
+import ImageViewer from '../../components/ImageViewer';
+import Button from '../../components/Button';
 
-export default function HomeScreen() {
+// import d'une image
+const PlaceholderImage = require('../../assets/images/profil.png');
+
+// fonction par défaut
+export default function App() 
+{
+	const [selectedImage, setSelectedImage] = useState(null);
+
+	const pickImageAsync = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			allowsEditing: true,
+			quality: 1,
+		});
+		
+		if (!result.canceled) {
+			setSelectedImage(result.assets[0].uri);
+		} else {
+			alert('You did not select any image.');
+		}
+	};
+	
     return (
-        <View style={styles.container}>
-          <Text>Page Profil</Text>
-        </View>
-      );
-  }
-
+		<View style={styles.container}>
+			<View style={styles.imageContainer}>
+			<ImageViewer
+          		placeholderImageSource={PlaceholderImage}
+          		selectedImage={selectedImage}
+        	/>
+			</View>
+			<View style={styles.footerContainer}>
+				<Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+				<Button label="Use this photo" theme={undefined} onPress={undefined} />
+			</View>
+			<StatusBar style="auto" />
+		</View>
+    );
+}
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-},
+	container: {
+		flex: 1,
+		backgroundColor: '#25292e',
+		alignItems: 'center',
+	},
+	imageContainer: {
+		flex: 1,
+		paddingTop: 58,
+	},
+	image: {
+		width: 320,
+		height: 440,
+		borderRadius: 18,
+	},
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: 'center',
+	},
 });
