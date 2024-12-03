6587976519edd0af7971cd68982b78a61efd5a7d
dadao34 <lucas.charasse@yahoo.fr>
2024-12-03 12:01:20 +0100

    correction pb back + début de correction pb image API

--- frontend/booktime/components/LivreRecherche.tsx
+++ frontend/booktime/components/LivreRecherche.tsx
@@ -3,11 +3,43 @@ import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { ThemedText } from './ThemedText';
 import { Book, BookInfos } from '@/models/Book';
 import { Book2 } from '@/models/Book2';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const defaultCover = require('@/assets/images/logo_refait.png');
 
+async function getImageAPI(book: BookInfos): Promise<string | number> {
+  if (book.thumbnail) {
+    try {
+      const response = await axios.get(book.thumbnail);
+      if (response.status === 200) {
+        return response.data; // Assurez-vous que response.data correspond à un type Image
+      } else {
+        return defaultCover; // Par défaut si le statut n'est pas 200
+      }
+    } catch (e) {
+      console.log(e);
+      return defaultCover; // En cas d'erreur dans la requête
+    }
+  } else {
+    return defaultCover; // Si aucun thumbnail n'est fourni
+  }
+}
+
 export const LivreRecherche = ({ book }: { book: BookInfos }) => {
+
+  const [imageSource, setImageSource] = useState<string | number>(defaultCover);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const image = await getImageAPI(book);
+      setImageSource(image);
+    };
+
+    fetchImage().catch((error) => {
+      console.error('Error loading image:', error);
+    });
+  }, [book]);
 
   const handleAddBook = () => {
     console.log(`Book added: ${book}`);
@@ -16,7 +48,7 @@ export const LivreRecherche = ({ book }: { book: BookInfos }) => {
   return (
     <View style={styles.itemContainer}>
 
-      <Image source={book.thumbnail ? { uri: book.thumbnail } : defaultCover} style={styles.itemImage} resizeMode={'cover'}></Image>
+      <Image source={getImageAPI(book)} style={styles.itemImage} resizeMode={'cover'}></Image>
       <View style={styles.itemInfos}>
         <ThemedText type="titreLivreHorizontal" numberOfLines={1}>{book.title}</ThemedText>
         <ThemedText type="auteurLivreHorizontal">{book.authors ? book.authors[0] : "Inconnue"}</ThemedText>
@@ -44,11 +76,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderColor: "#1E9AA4",
     borderWidth: 1,
-    flex:2,
+    flex: 2,
   },
   itemInfos: {
     padding: 10,
-    flex:6,
+    flex: 6,
   },
   titreItem: {
     fontSize: 18,
@@ -61,7 +93,7 @@ const styles = StyleSheet.create({
   addItemContainer: {
     alignItems: "center",
     justifyContent: "center",
-    flex:1,
+    flex: 1,
   },
   addItem: {
     width: 40,
