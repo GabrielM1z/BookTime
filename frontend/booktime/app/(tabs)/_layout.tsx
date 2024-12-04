e13e857f9f70ad8e03551267fbe5cc2fcae3cbaa
GabrielM1z <gabazoo.martinez@gmail.com>
2024-12-04 09:50:53 +0100

    front

--- frontend/booktime/app/(tabs)/_layout.tsx
+++ frontend/booktime/app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ export default function TabLayout() {
 	return (
 		<Tabs
 		screenOptions={{
-			tabBarActiveTintColor: "blue",
+			tabBarActiveTintColor: Colors.dark.background,
 			headerShown: false,
 			tabBarShowLabel: false,
 			tabBarStyle: {
