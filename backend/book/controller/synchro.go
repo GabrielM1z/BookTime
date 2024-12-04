d9198315cc6afa7e27d884f21eb6530ffa4ada41
Rexmicool <matthieu.barraud@gmail.com>
2024-12-04 19:51:29 +0100

    receive and mix actions

--- backend/book/controller/synchro.go
+++ backend/book/controller/synchro.go
@@ -1,15 +1,14 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
-	"time"
 
 	"book/controller/interfaces"
 	"book/model"
 	"book/service"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 type synchroController struct {
@@ -22,29 +21,21 @@ func NewSynchroController(SynchroService *service.SynchroService) *synchroContro
 
 func (bc *synchroController) Synchro(c *gin.Context) {
 
-	a1 := model.Action{
-		IdAction:   uuid.New(),
-		IdUser:     uuid.New(),
-		Table:      "books",
-		Date:       time.Now(),
-		Type:       "update",
-		Action:     []byte("Increment read count"),
-		ExecutedBy: "system",
+	jsonData := c.Query("jsonData")
+	if jsonData == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "jsonData is required"})
+		return
 	}
 
-	a2 := model.Action{
-		IdAction:   uuid.New(),
-		IdUser:     uuid.New(),
-		Table:      "libraries",
-		Date:       time.Now(),
-		Type:       "create",
-		Action:     []byte("Added new book to library"),
-		ExecutedBy: "admin_user",
+	// Convertir la chaîne JSON en slice d'Action
+	var actions []model.Action
+	err := json.Unmarshal([]byte(jsonData), &actions)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON format: " + err.Error()})
+		return
 	}
-	//recupe donnee
-	actions_from_client := []model.Action{a1, a2}
 
-	actions_to_exec, err := bc.SynchroService.Synchro(actions_from_client)
+	actions_to_exec, err := bc.SynchroService.Synchro(actions)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
