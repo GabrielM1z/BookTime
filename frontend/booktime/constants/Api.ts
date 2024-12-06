56b7c723246ce5fc6d680878de6fa44487c73654
Jean Guyomarc'h <jeang120@outlook.fr>
2024-12-06 15:12:34 +0100

    ajout dotenv

--- frontend/booktime/constants/Api.ts
+++ frontend/booktime/constants/Api.ts
@@ -3,10 +3,12 @@ export const apiLink = "https://www.googleapis.com/books/v1/volumes?" + key;
 
 // export const backendIp = "159.31.247.130";
 // export const keycloakAuthUrl = "http://" + backendIp + ":8080/realms/booktime/protocol/openid-connect/token";
-export const keycloakClientSecret = "szM12JEwNGOLLUuLnIRfYEdEqcH1LDFq";
-export const keycloakClientId = "gateway-client";
 // export const apiLinkServeur = "http://159.31.247.130:8082/books/search/?";
 
-export const baseURL = "http://booktime.ddns.net";
+export const keycloakClientSecret = process.env.KEYCLOAK_CLIENT_SECRET || "szM12JEwNGOLLUuLnIRfYEdEqcH1LDFq";
+export const keycloakClientId = process.env.KEYCLOAK_CLIENT_ID || "gateway-client";
+
+export const baseURL = process.env.REACT_APP_BACKEND_URL || "http://booktime.ddns.net";
+
 export const keycloakAuthUrl = baseURL +  "/auth/realms/booktime/protocol/openid-connect/token";
 export const apiLinkServeur = baseURL + "/api/books/search/?";
