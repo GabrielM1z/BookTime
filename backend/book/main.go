fc348abe6fc1fa27df51c550c8e0dc04538e8f0c
dadao34 <lucas.charasse@yahoo.fr>
2025-01-13 21:50:53 +0100

    ajout de fichier de logs

--- backend/book/main.go
+++ backend/book/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"book/app"
+	"io"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	f, _ := os.Create("log/gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
 	var a app.App
 	a.CreateConnection()
 	// a.Migrate()
