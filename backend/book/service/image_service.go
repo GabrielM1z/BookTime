7e11d4f71f016f1d5f970a78b9a675e1e0e48607
Rexmicool <matthieu.barraud@gmail.com>
2025-02-11 16:36:21 +0100

    Fix filename generation in image service to use a static book ID

--- backend/book/service/image_service.go
+++ backend/book/service/image_service.go
@@ -38,7 +38,7 @@ func (i *ImageService) SaveBookImage(imageURL string, bookID string) (string, er
 
 	// Détermine le chemin du fichier
 	//filename := fmt.Sprintf("book_%d.jpg", bookID)
-	filename := fmt.Sprintf("book_12345.jpg", bookID)
+	filename := fmt.Sprintf("book_12345.jpg")
 	filePath := filepath.Join(storagePath, filename)
 
 	// Crée le fichier localement
