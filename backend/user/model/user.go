32df7583419076e7ec646e778fa2c21fe8021320
MaelJEAN <mael.jean@etu.mines-ales.fr>
2025-02-14 08:49:21 +0100

    Renommer les clés JSON pour les utilisateurs

--- backend/user/model/user.go
+++ backend/user/model/user.go
@@ -3,13 +3,13 @@ package model
 import "github.com/google/uuid"
 
 type User struct {
-	IdUser      uuid.UUID `json:"idUser"`
+	IdUser      uuid.UUID `json:"id_user"`
 	Name        string    `json:"name"`
 	Email       string    `json:"email"`
 	Pseudo      string    `json:"pseudo"`
 	Description string    `json:"description"`
 	Private     bool      `json:"private"`
-	ProfilImage string    `json:"profilImage"`
-	BannerImage string    `json:"bannerImage"`
+	ProfilImage string    `json:"profil_image"`
+	BannerImage string    `json:"banner_image"`
 	Birthday    string    `json:"birthdate"`
 }
