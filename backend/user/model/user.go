699bb888c2b3bb9d9f1bd01e146942ece41439d3
Rexmicool <matthieu.barraud@gmail.com>
2025-02-17 14:32:49 +0100

    Refactor user data handling: introduce KeyUser struct and update GetUserFromToken response

--- backend/user/model/user.go
+++ backend/user/model/user.go
@@ -4,6 +4,16 @@ import "github.com/google/uuid"
 
 type User struct {
 	IdUser      uuid.UUID `json:"idUser"`
+	Pseudo      string    `json:"pseudo"`
+	Description string    `json:"description"`
+	Private     bool      `json:"private"`
+	ProfilImage string    `json:"profilImage"`
+	BannerImage string    `json:"bannerImage"`
+	Birthday    string    `json:"birthdate"`
+}
+
+type KeyUser struct {
+	IdUser      uuid.UUID `json:"idUser"`
 	Name        string    `json:"name"`
 	Email       string    `json:"email"`
 	Pseudo      string    `json:"pseudo"`
