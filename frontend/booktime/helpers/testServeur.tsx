0ffc0b1c8b435b159b2780a035aeadd697ffce49
Jean Guyomarc'h <jeang120@outlook.fr>
2025-02-19 21:23:20 +0100

    ca marche toujours pas

--- frontend/booktime/helpers/testServeur.tsx
+++ frontend/booktime/helpers/testServeur.tsx
@@ -1,11 +1,29 @@
 import { Alert } from "react-native";
-import { keycloak } from "@/services/axios";
-
+import { baseURL } from "@/constants/Api";
+import axios from "axios";
 
 export const testServeur = async () => {
     if (__DEV__) {
-        await keycloak.head("").catch((error) => {
-            console.error(error);
+        await axios.head(baseURL).catch((error) => {
+            if (error.response) {
+
+                // The server responded with a status code outside the 2xx range
+          
+                console.log('Error response:', error.response);
+          
+              } else if (error.request) {
+          
+                // The request was made but no response was received
+          
+                console.log('Error request:', error.request);
+          
+              } else {
+          
+                // Something happened in setting up the request that triggered an error
+          
+                console.log('Error message:', error.message);
+          
+              }
             Alert.alert(
                 "Serveur non disponible",
                 "Le serveur n'est pas disponible. Veuillez vérifier votre connexion internet.",
