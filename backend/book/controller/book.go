46c26f468e3e190fb221a4fc6795e15f1a27e63d
Rexmicool <matthieu.barraud@gmail.com>
2025-02-20 10:50:57 +0100

    add support for Oracle environment in book controller and update Oracle.bru to use undefined tokens

--- backend/book/controller/book.go
+++ backend/book/controller/book.go
@@ -63,6 +63,8 @@ func (bc *BookController) GetBook(c *gin.Context) {
 		publicURLBase := "http://159.31.247.130:8082/books"
 		if os.Getenv("ENVIROMENT") == "development" {
 			publicURLBase = "http://localhost:8082/books"
+		} else if os.Getenv("ENVIROMENT") == "oracle" {
+			publicURLBase = "http://84.235.239.52:8082/books"
 		}
 		// fmt.Println("le publicURLBase est:", publicURLBase)
 
